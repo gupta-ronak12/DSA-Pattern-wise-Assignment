7863cd51243dd499f97909bff71c0daa4fcd55bb
gupta-ronak12 <ronak.gupta2427@gmail.com>
2026-02-19 21:27:26 +0530

    Create reverse-array.py
    Reverse an array

---
+++ Array/reverse-array.py
@@ -0,0 +1,18 @@
+# Question: Reverse an array
+
+def reverseArray(arr):
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+
+    return arr
+
+
+if __name__ == "__main__":
+    arr = [1, 4, 3, 2, 6, 5]
+    reverseArray(arr)
+    print(*arr)
